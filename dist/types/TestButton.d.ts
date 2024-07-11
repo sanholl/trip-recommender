2f8e2c49c15243dc5e5b34f02c069e513bf24fba
Hansol Jang <edgar.jang93@hanmail.net>
2024-07-11 11:13:26 +0900

    chore: npm i -D @babel/preset-typescript

--- dist/types/TestButton.d.ts
+++ dist/types/TestButton.d.ts
@@ -1,8 +1,5 @@
 import React from 'react';
 import './TestButton.styles.scss';
-type TestButtonProps = {
-    label: string;
-    onClick: () => void;
-};
-declare const TestButton: (props: TestButtonProps) => React.JSX.Element;
+import { TestButtonProps } from 'trip-recommender';
+declare const TestButton: ({ label, onClick }: TestButtonProps) => React.JSX.Element;
 export default TestButton;
