003b9c9bece7820c75a73bbc5e202d418d413e31
Hansol Jang <edgar.jang93@hanmail.net>
2024-07-16 11:04:23 +0900

    Remove dist from git tracking

--- dist/types/TestButton.d.ts
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import './TestButton.styles.scss';
-import { TestButtonProps } from 'trip-recommender';
-declare const TestButton: ({ label, onClick }: TestButtonProps) => React.JSX.Element;
-export default TestButton;
--- dist/types/index.d.ts
+++
@@ -1,1 +0,0 @@
-export { default as TestButton } from "./TestButton.js";
