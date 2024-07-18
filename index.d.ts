5a5b8e2af2adebb5bfcd9d9198f5b774354eb71a
Hansol Jang <edgar.jang93@hanmail.net>
2024-07-18 16:31:57 +0900

    chore: 필요 component 생성 및 index.d.ts 수정

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,21 @@
 declare module 'trip-recommender' {
+  // import { ReactNode } from 'react';
+  export interface PlaceType {
+    id: string
+    position: kakao.maps.LatLng
+    title: string
+    address: string
+  }
+
   export interface TestButtonProps {
-    label: string,
-    onClick: () => void
+    label: string;
+    onClick: () => void;
   }
   export const TestButton: React.FC<TestButtonProps>;
+
+  export interface SearchLocationProps {
+    onUpdatePlaces: (places: PlaceType[]) => void;
+    onSelect: (placeId: string) => void;
+  }
+  export const SearchLocation: React.FC<SearchLocationProps>;
 }
---
+++ src/app/index.ts
@@ -0,0 +1,2 @@
+export { default as SearchLocation } from '../widgets/SearchLocation';
+export { default as TestButton } from '../TestButton';
