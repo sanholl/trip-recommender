f8258d2ae6e13c8f4c7d2dc3978f42bcf5001386
sanholl <edgar.jang93@hanmail.net>
2024-07-20 19:28:03 +0900

    chore: 필요없는 컴포넌트 삭제

--- index.d.ts
+++ index.d.ts
@@ -1,61 +1,7 @@
 declare module 'trip-recommender' {
-  export interface PlaceType {
-    id: string
-    position: kakao.maps.LatLng
-    title: string
-    address: string
-  }
-  export type PlaceState = {
-    selectedPlace: PlaceType | null;
-  };
-  
-  export type PlaceAction =
-    | { type: 'SET_PLACE'; payload: PlaceType }
-    | { type: 'CLEAR_PLACE' };
-  export interface PlaceProviderProps {
-    children: ReactNode
-  }
-  export const PlaceProvider: React.FC<PropsWithChildren<PlaceProviderProps>>;
-  
-  export interface DynamicMapProps {
-    children: ReactNode
-  }
-  export const DynamicMap: React.FC<PropsWithChildren<DynamicMapProps>>;
-  
-  export interface KakaoMapScriptLoaderProps {
-    children: ReactNode,
-    kakaoMapAppKey: string
-  }
-  export const KakaoMapScriptLoader: React.FC<PropsWithChildren<KakaoMapScriptLoaderProps>>;
-  
-  export interface MapMarkerProps {
-    place: PlaceType
-    index: number
-    showInfo?: boolean
-  }
-  export const MapMarker: React.FC<MapMarkerProps>;
-  
-  export interface MapMarkerControllerProps {
-    places: PlaceType[]
-    selectedPlaceId: string
-  }
-  export const MapMarkerController:  React.FC<MapMarkerControllerProps>;
-  
-  export interface SearchLocationProps {
-    onUpdatePlaces: (places: PlaceType[]) => void;
-    onSelect: (placeId: string) => void;
-  }
-  export const SearchLocation: React.FC<SearchLocationProps>;
-  
   export interface ChatResponseProps {
-    openAiKey: string
+    keyword: string;
+    openAiKey: string;
   }
   export const ChatResponse: React.FC<ChatResponseProps>;
-  
-  export interface SidebarContainerProps {
-    places: PlaceType[];
-    setPlaces: React.Dispatch<React.SetStateAction<PlaceType[]>>;
-    setSelectedPlaceId: React.Dispatch<React.SetStateAction<string>>;
-  }
-  export const SidebarContainer: React.FC<SidebarContainerProps>;
 }
--- src/features/ChatGpt/ui/ChatResponse/index.tsx
+++ src/features/ChatGpt/ui/ChatResponse/index.tsx
@@ -1,28 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { chatResponse } from "../../api/ChatGptApi";
 import { Container, Title, List } from "./ChatResponse.styles";
-import { usePlaceState } from "../../../../entities/KakaoMap/lib/context/PlaceProvider";
 import { ChatResponseProps } from "trip-recommender";
 
-const ChatResponse = ({ openAiKey }: ChatResponseProps) => {
+const ChatResponse = ({ keyword, openAiKey }: ChatResponseProps) => {
   const [isLoading, setLoading] = useState<boolean>(false);
   const [recommendation, setRecommendation] = useState<string | null>(null);
-  const placeState = usePlaceState();
-  const place = placeState.selectedPlace;
 
   useEffect(() => {
-    if (place && place.address) {
-      fetchResponse();
-    }
-  }, [place]);
+    fetchResponse();
+  }, [keyword]);
 
   const fetchResponse = async () => {
     setLoading(true);
     try {
-      if (place && place.address) {
-        const response = await chatResponse(place.address, openAiKey);
-        setRecommendation(response);
-      }
+      const response = await chatResponse(keyword, openAiKey);
+      setRecommendation(response);
     } catch (error) {
       console.error("send message error", error);
       setRecommendation("추천을 가져오는 중 문제가 발생했습니다.");
